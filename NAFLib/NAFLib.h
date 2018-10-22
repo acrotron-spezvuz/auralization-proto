1d92f5caf98293edeedcc11acb71055ab7bc2b93
Stanislav Mishchenko <mishchenko.stanislav@gmail.com>
2018-10-22 20:17:39 +0300

    Auralization method

--- NAFLib/NAFLib.h
+++ NAFLib/NAFLib.h
@@ -1,1 +1,7 @@
 #pragma once
+#include "SoundSource.h"
+
+#define DllExport   __declspec( dllexport )
+
+// Auralization selected sound sources
+extern "C" DllExport char* __stdcall Auralization(SoundSource sources[]);
