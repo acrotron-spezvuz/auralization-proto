839acc577b8aee78538bff7e05ca7788337dfe8b
Stanislav Mishchenko <mishchenko.stanislav@gmail.com>
2018-10-22 20:48:57 +0300

    ASP.Net web api project

---
+++ Auralization.API/Controllers/AuralizarionController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auralization.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuralizarionController : ControllerBase
+    {
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
