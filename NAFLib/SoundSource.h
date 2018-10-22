ca4e6ae08f1a0ab07647388a581bffef78df6676
Stanislav Mishchenko <mishchenko.stanislav@gmail.com>
2018-10-22 19:39:21 +0300

    Sound source props

--- NAFLib/SoundSource.h
+++ NAFLib/SoundSource.h
@@ -1,6 +1,14 @@
 #pragma once
+#include "SourceTypes.h"
 
 // Sound source struct
 typedef struct {
 public:	
+	SourceTypes source;
+	double height;
+	double lat;
+	double lon;
+	double Vx;
+	double Vy;
+	double Vz;	
 } SoundSource;
