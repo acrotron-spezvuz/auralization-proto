b8da41d44dbc17c8716f975a4bdbe9a3b0f4d60f
Stanislav Mishchenko <mishchenko.stanislav@gmail.com>
2018-10-23 13:28:27 +0300

    Added NLog logger

--- Auralization.API/Controllers/AuralizarionController.cs
+++ Auralization.API/Controllers/AuralizarionController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Auralization.API.Controllers
 {
@@ -10,10 +11,18 @@ namespace Auralization.API.Controllers
     [ApiController]
     public class AuralizarionController : ControllerBase
     {
+        readonly ILogger _logger;
+
+        public AuralizarionController(ILogger<AuralizarionController> logger)
+        {
+            _logger = logger;
+        }
+
         // POST api/values
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            _logger.LogDebug("Auralization ...");
         }
     }
 }
--- Auralization.API/Startup.cs
+++ Auralization.API/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using NLog.Extensions.Logging;
+using NLog.Web;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace Auralization.API
@@ -40,14 +42,16 @@ namespace Auralization.API
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
             });
+
+            services.AddLogging();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger();
-
+            app.UseSwagger();                        
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -56,7 +60,7 @@ namespace Auralization.API
             {
                 app.UseHsts();
             }
-
+            
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
             // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
@@ -66,7 +70,7 @@ namespace Auralization.API
             });
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseMvc();           
         }
     }
 }
