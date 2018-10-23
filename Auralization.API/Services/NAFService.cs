b2ea1883f62a09e7703c84600ccefc2afedfec2f
Stanislav Mishchenko <mishchenko.stanislav@gmail.com>
2018-10-23 15:36:55 +0300

    NAF Service

---
+++ Auralization.API/Services/NAFService.cs
@@ -0,0 +1,21 @@
+﻿using Auralization.API.Models;
+using System.Runtime.InteropServices;
+
+namespace Auralization.API.Services
+{
+    /// <summary>
+    /// NAF Service 
+    /// </summary>
+    internal static class NAFService
+    {
+        /// <summary>
+        /// Auralization of sound sources
+        /// </summary>
+        /// <param name="sources"></param>
+        /// <returns></returns>
+        [DllImport("NAFLib", CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.LPStr)]
+        public static extern string Auralization(SoundSource[] sources);
+
+    }
+}
