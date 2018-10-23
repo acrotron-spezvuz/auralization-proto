ea0f899e8139482a0d754d9b3e38fdbe6b1c8c8a
Stanislav Mishchenko <mishchenko.stanislav@gmail.com>
2018-10-23 14:03:32 +0300

    Added SoundTypes enum

---
+++ Auralization.API/Models/SoundTypes.cs
@@ -0,0 +1,14 @@
+﻿namespace Auralization.API.Models
+{
+    /// <summary>
+    /// Types of the sound source
+    /// </summary>
+    public enum SoundTypes
+    {
+        Car = 0,
+        Truck,
+        Bus,
+        Airplane,
+        Helicopter
+    }
+}
