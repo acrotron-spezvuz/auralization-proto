8985251cc8dc5411faf5d3ed445f4e6848dba08e
Stanislav Mishchenko <mishchenko.stanislav@gmail.com>
2018-10-23 15:53:38 +0300

    ApiResponse generic model

---
+++ Auralization.API/ApiResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auralization.API
+{
+    /// <summary>
+    /// Default api response wrapper
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ApiResponse<T>
+    {
+        /// <summary>
+        /// Data for client
+        /// </summary>
+        public T Data { get; set; }
+        
+        /// <summary>
+        ///  default constructor
+        /// </summary>
+        public ApiResponse(){}
+
+        /// <summary>
+        /// Init reponse with data
+        /// </summary>
+        /// <param name="data"></param>
+        public ApiResponse(T data) => Data = data;
+    }
+}
