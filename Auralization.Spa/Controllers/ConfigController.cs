85206788e76445158561f03e24716110399dacc3
Stanislav Mishchenko <mishchenko.stanislav@gmail.com>
2018-10-28 20:55:02 +0200

    SPA: api controller for config

---
+++ Auralization.Spa/Controllers/ConfigController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace Auralization.Spa.Controllers
+{
+    /// <summary>
+    /// Configuration controller
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigController : ControllerBase
+    {
+        /// <summary>
+        /// Api settings
+        /// </summary>
+        readonly IOptionsSnapshot<ApiSettings> _apiSettings;
+
+        public ConfigController(IOptionsSnapshot<ApiSettings> apiSettings)
+        {
+            // inject
+            _apiSettings = apiSettings;
+        }
+
+        /// <summary>
+        /// get config
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Config()
+        {
+            return Ok(_apiSettings.Value);
+        }
+    }
+}
