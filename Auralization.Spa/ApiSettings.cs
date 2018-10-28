f4d6568e2a51bd078e8ef21a2d3787bf77018b72
Stanislav Mishchenko <mishchenko.stanislav@gmail.com>
2018-10-28 20:50:05 +0200

    SPA: api settings file

---
+++ Auralization.Spa/ApiSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auralization.Spa
+{
+    /// <summary>
+    /// Api configuration
+    /// </summary>
+    public class ApiSettings
+    {
+        /// <summary>
+        /// auralization api endpoint
+        /// </summary>
+        public string AuralizaitionEndpoint { get; set; }
+
+    }
+}
