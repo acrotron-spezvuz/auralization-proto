2cb048a1d4e642968db7984f0fcb859818d9cf03
Stanislav Mishchenko <mishchenko.stanislav@gmail.com>
2018-10-28 20:52:17 +0200

    SPA: Read configuration into class

--- Auralization.Spa/Startup.cs
+++ Auralization.Spa/Startup.cs
@@ -5,16 +5,31 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace Auralization.Spa
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddOptions();
+            services.Configure<ApiSettings>(Configuration.GetSection("ApiSettings"));
+            services.AddTransient(sp => sp.GetService<IOptionsSnapshot<ApiSettings>>().Value);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +42,8 @@ namespace Auralization.Spa
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
+
+            app.UseMvc();
         }
     }
 }
